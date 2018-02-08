9c984ab30f3979541887c8258254b8ff0e29ef90
Gareth Smith <gsmith@pivotal.io>
2018-02-08 16:12:15 +0000

    Remove our explicit constraint on C
    Now we can allow our implicit constraint on C through B to dominate.
    `dep ensure` works. We get version 2 of C everywhere, and the compatible
    version of B everywhere. So `go run main.go` prints only ostriches, and
    `go test ./src/a/vendor/github.com/totherme/govendorexplorationb`
    passes.

--- src/a/vendor/github.com/totherme/govendorexplorationb/thing_test.go
+++ src/a/vendor/github.com/totherme/govendorexplorationb/thing_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestBFunc(t *testing.T) {
-	if b.BFunc() != "Badgers" {
+	if b.BFunc() != "Ostriches" {
 		t.Fail()
 	}
 }
--- src/a/vendor/github.com/totherme/govendorexplorationc/otherthing.go
+++ src/a/vendor/github.com/totherme/govendorexplorationc/otherthing.go
@@ -1,5 +1,5 @@
 package govendorexplorationc
 
 func CFunc() string {
-	return "Badgers"
+	return "Ostriches"
 }
